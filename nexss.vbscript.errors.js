08dcf4e758699a233b71923fb0b5ceef541152a8
Marcin Polak <mapoart@gmail.com>
2020-01-15 20:51:03 +0100

    Added Nexss Programmer Installation Info

--- nexss.vbscript.errors.js
+++ nexss.vbscript.errors.js
@@ -1,7 +1,4 @@
 module.exports = {
-  win32: {
-    "ModuleNotFoundError: No module named '(.*?)'":
-      "nexss install py <module> OR pip3 install <module>",
-    "not found": `nexss install py <module> OR pip3 install <module>`
-  }
+  "ModuleNotFoundError: No module named '(.*?)'":
+    "define your solution here <found1>"
 };
